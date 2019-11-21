f38d867616410d2855f08ef79b0911ed86a20c0e
Raz Levy <35895857+RLeDEV@users.noreply.github.com>
2019-11-21 21:11:53 +0200

    Create linkedinScrap.py
    I've made a complete file to scrap LinkedIn, I've also checked if it's necessary to use Selenium to simulate login proccess and it's not since you can access the jobs page also when you're not logged in.
    I've used urllib, regex and BeautifulSoup4 to scrap the page.
    
    A sample to use : 
    `
    soup = scrapLinkedin.scrap('https://www.linkedin.com/jobs/search/?keywords=student%20developer&location=Israel')
    jobs = scrapLinkedin.getJobs(soup)
    scrapLinkedin.printJobs(jobs)
    `

---
+++ linkedinScrap.py
@@ -0,0 +1,35 @@
+import urllib.request
+import re
+from bs4 import BeautifulSoup
+
+def scrap(jobsURL):
+    try:
+        page = urllib.request.urlopen(jobsURL)
+        soup = BeautifulSoup(page, 'html.parser')
+        return soup
+    except:
+        print('Error while scraping information from LinkedIn')
+
+def getJobs(soup):
+    if soup is not None:
+        jobTitleRegex = re.compile('^result-card__full-card-link')
+        jobsList = soup.find_all('a', attrs={'class': jobTitleRegex})
+        companyTitleRegex = re.compile('COMPANY-*')
+        companysList = soup.find_all('label', attrs={'for': companyTitleRegex})
+        linksList = []
+        for link in jobsList:
+            linksList.append(link.get('href'))
+        finalList = []
+        for job,company,link in zip(jobsList,companysList,linksList):
+            finalList.append('Job Title: ' + str(job.getText()) + ',Comapny: ' + str(company.getText()) + ', Link:' + link)
+        return finalList
+    else:
+        return None
+
+def printJobs(finalList):
+    if finalList is not None:
+        for job in finalList:
+            print(job)
+        print('DONE!')
+    else:
+        return None
